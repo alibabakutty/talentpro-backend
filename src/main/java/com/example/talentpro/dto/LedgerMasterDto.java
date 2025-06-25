522f1b1a608322e05fe952a672279c2a1553cdb0
alibabakutty <tallysriram007@gmail.com>
2025-06-25 18:18:45 +0530

    change for total amount fields

--- src/main/java/com/example/talentpro/dto/LedgerMasterDto.java
+++ src/main/java/com/example/talentpro/dto/LedgerMasterDto.java
@@ -3,7 +3,7 @@ package com.example.talentpro.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,6 +13,14 @@ import java.util.List;
 public class LedgerMasterDto {
 
     private LocalDateTime ledgerTime;
+
+    private BigDecimal totalDebitAmount;
+
+    private BigDecimal totalCreditAmount;
+
+    private BigDecimal totalCostCenterAmountDebit;
+
+    private BigDecimal totalCostCenterAmountCredit;
 
     private List<LedgerTransactionsDto> ledgerTransactions;
 }
--- src/main/java/com/example/talentpro/entity/LedgerMaster.java
+++ src/main/java/com/example/talentpro/entity/LedgerMaster.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -21,6 +23,18 @@ public class LedgerMaster {
 
     @Column(name = "ledger_date")
     private LocalDateTime ledgerTime;
+
+    @Column(name = "total_debit_amount")
+    private BigDecimal totalDebitAmount;
+
+    @Column(name = "total_credit_amount")
+    private BigDecimal totalCreditAmount;
+
+    @Column(name = "total_cost_center_amount_debit")
+    private BigDecimal totalCostCenterAmountDebit;
+
+    @Column(name = "total_cost_center_amount_credit")
+    private BigDecimal totalCostCenterAmountCredit;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     @JoinColumn(name = "transaction_id")
